5f662e46b60991c8f7012d0b3d96ebd0d60e3727
woojuan <dnwn010194@gmail.com>
2020-12-12 20:25:23 +0900

    페이스북 로그인

--- app/src/main/java/com/example/instagram/LoginActivity.kt
+++ app/src/main/java/com/example/instagram/LoginActivity.kt
@@ -10,22 +10,31 @@ import android.util.Log
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.facebook.AccessToken
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
 import com.google.android.gms.auth.api.Auth
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
 import java.security.MessageDigest
 import java.security.NoSuchAlgorithmException
+import java.util.*
 
 
 class LoginActivity : AppCompatActivity() {
     var auth: FirebaseAuth? = null
-    var googleSignInClient : GoogleSignInClient? = null
+    var googleSignInClient: GoogleSignInClient? = null
     var GOOGLE_LOGIN_CODE = 9001
+    var callbackManager: CallbackManager? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -39,12 +48,18 @@ class LoginActivity : AppCompatActivity() {
             // First step
             googleLogin()
         }
+        val facebook_login_button: android.widget.Button = findViewById(R.id.facebook_login_button)
+        facebook_login_button.setOnClickListener{
+            // First step
+            facebookLogin()
+        }
         var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
             .build()
         googleSignInClient = GoogleSignIn.getClient(this, gso)
-        printHashKey()
+        //printHashKey()
+        callbackManager = CallbackManager.Factory.create()
     }
 
     fun printHashKey() {
@@ -64,17 +79,55 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
-    fun googleLogin(){
+    fun googleLogin() {
         var signInIntent = googleSignInClient?.signInIntent
         startActivityForResult(signInIntent, GOOGLE_LOGIN_CODE)
     }
 
+    fun facebookLogin() {
+        LoginManager.getInstance()
+            .logInWithPublishPermissions(this, Arrays.asList("public_profile", "email"))
+
+        LoginManager.getInstance()
+            .registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
+                override fun onSuccess(result: LoginResult?) {
+                    // Second step
+                    handleFacebookAccessToken(result?.accessToken)
+                }
+
+                override fun onCancel() {
+
+                }
+
+                override fun onError(error: FacebookException?) {
+
+                }
+
+            })
+    }
+
+    fun handleFacebookAccessToken(token: AccessToken?) {
+        var credential = FacebookAuthProvider.getCredential(token?.token!!)
+        auth?.signInWithCredential(credential)
+            ?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    // Third step
+                    // Login
+                    moveMainPage(task.result?.user)
+                } else {
+                    // Show the error message
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode == GOOGLE_LOGIN_CODE){
+        callbackManager?.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == GOOGLE_LOGIN_CODE) {
             var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
             if (result != null) {
-                if(result.isSuccess) {
+                if (result.isSuccess) {
                     var account = result?.signInAccount
                     //Second step
                     firebaseAuthWithGoogle(account)
@@ -82,7 +135,8 @@ class LoginActivity : AppCompatActivity() {
             }
         }
     }
-    fun firebaseAuthWithGoogle(account: GoogleSignInAccount?){
+
+    fun firebaseAuthWithGoogle(account: GoogleSignInAccount?) {
         var credential = GoogleAuthProvider.getCredential(account?.idToken, null)
         auth?.signInWithCredential(credential)
             ?.addOnCompleteListener { task ->
@@ -97,8 +151,8 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun signinAndSignup() {
-        val email_edittext : EditText = findViewById(R.id.email_edittext)
-        val password_edittext : EditText = findViewById(R.id.password_edittext)
+        val email_edittext: EditText = findViewById(R.id.email_edittext)
+        val password_edittext: EditText = findViewById(R.id.password_edittext)
 
         auth?.createUserWithEmailAndPassword(
             email_edittext.text.toString(),
@@ -119,8 +173,8 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun signinEmail() {
-        val email_edittext : EditText = findViewById(R.id.email_edittext)
-        val password_edittext : EditText = findViewById(R.id.password_edittext)
+        val email_edittext: EditText = findViewById(R.id.email_edittext)
+        val password_edittext: EditText = findViewById(R.id.password_edittext)
 
         auth?.signInWithEmailAndPassword(
             email_edittext.text.toString(),
@@ -136,8 +190,9 @@ class LoginActivity : AppCompatActivity() {
                 }
             }
     }
-    fun moveMainPage(user: FirebaseUser?){
-        if(user != null){
+
+    fun moveMainPage(user: FirebaseUser?) {
+        if (user != null) {
             startActivity(Intent(this, MainActivity::class.java))
         }
     }
