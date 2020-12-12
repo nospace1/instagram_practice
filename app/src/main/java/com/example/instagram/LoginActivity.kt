cc85a1464e3d0560e5769fe72808ab93daf28270
woojuan <dnwn010194@gmail.com>
2020-12-12 20:01:58 +0900

    페이스북 로그인

--- app/src/main/java/com/example/instagram/LoginActivity.kt
+++ app/src/main/java/com/example/instagram/LoginActivity.kt
@@ -1,24 +1,99 @@
 package com.example.instagram
 
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Base64
+import android.util.Log
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
 
 
 class LoginActivity : AppCompatActivity() {
     var auth: FirebaseAuth? = null
+    var googleSignInClient : GoogleSignInClient? = null
+    var GOOGLE_LOGIN_CODE = 9001
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         auth = FirebaseAuth.getInstance()
-        val email_login_button : android.widget.Button = findViewById(R.id.email_login_button)
+        val email_login_button: android.widget.Button = findViewById(R.id.email_login_button)
         email_login_button.setOnClickListener {
             signinAndSignup()
         }
+        val google_sign_in_button: android.widget.Button = findViewById(R.id.google_singin_button)
+        google_sign_in_button.setOnClickListener {
+            // First step
+            googleLogin()
+        }
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+        printHashKey()
+    }
+
+    fun printHashKey() {
+        try {
+            val info = packageManager
+                .getPackageInfo(packageName, PackageManager.GET_SIGNATURES)
+            for (signature in info.signatures) {
+                val md: MessageDigest = MessageDigest.getInstance("SHA")
+                md.update(signature.toByteArray())
+                val hashKey: String = String(Base64.encode(md.digest(), 0))
+                Log.i("TAG", "printHashKey() Hash Key: $hashKey")
+            }
+        } catch (e: NoSuchAlgorithmException) {
+            Log.e("TAG", "printHashKey()", e)
+        } catch (e: Exception) {
+            Log.e("TAG", "printHashKey()", e)
+        }
+    }
+
+    fun googleLogin(){
+        var signInIntent = googleSignInClient?.signInIntent
+        startActivityForResult(signInIntent, GOOGLE_LOGIN_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == GOOGLE_LOGIN_CODE){
+            var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            if (result != null) {
+                if(result.isSuccess) {
+                    var account = result?.signInAccount
+                    //Second step
+                    firebaseAuthWithGoogle(account)
+                }
+            }
+        }
+    }
+    fun firebaseAuthWithGoogle(account: GoogleSignInAccount?){
+        var credential = GoogleAuthProvider.getCredential(account?.idToken, null)
+        auth?.signInWithCredential(credential)
+            ?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    // Login
+                    moveMainPage(task.result?.user)
+                } else {
+                    // Show the error message
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                }
+            }
     }
 
     fun signinAndSignup() {
@@ -32,10 +107,10 @@ class LoginActivity : AppCompatActivity() {
             ?.addOnCompleteListener { task ->
                 if (task.isSuccessful) {
                     // Creating a user account
-                    moveMainPage(task.result.user)
+                    moveMainPage(task.result?.user)
                 } else if (task.exception?.message.isNullOrEmpty()) {
                     // show the error message
-                    Toast.makeText(this, task.exception?.message,Toast.LENGTH_LONG).show()
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
                 } else {
                     // Login if you have account
                     signinEmail()
@@ -54,10 +129,10 @@ class LoginActivity : AppCompatActivity() {
             ?.addOnCompleteListener { task ->
                 if (task.isSuccessful) {
                     // Login
-                    moveMainPage(task.result.user)
+                    moveMainPage(task.result?.user)
                 } else {
                     // Show the error message
-                    Toast.makeText(this, task.exception?.message,Toast.LENGTH_LONG).show()
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
                 }
             }
     }
