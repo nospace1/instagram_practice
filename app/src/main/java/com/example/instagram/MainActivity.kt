c576a043fc5b1babd663698352e028caa40bbf0b
woojuan <dnwn010194@gmail.com>
2020-12-13 12:40:18 +0900

    사진 업로드 페이지 만들기

--- app/src/main/java/com/example/instagram/MainActivity.kt
+++ app/src/main/java/com/example/instagram/MainActivity.kt
@@ -1,24 +1,26 @@
 package com.example.instagram
 
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.LinearLayout
-import com.example.instagram.navigation.AlarmFragment
-import com.example.instagram.navigation.DetailViewFragment
-import com.example.instagram.navigation.GridFragment
-import com.example.instagram.navigation.UserFragment
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.instagram.navigation.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import java.util.jar.Manifest
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         //val bottom_navigation : LinearLayout = findViewById(R.id.bottom_navigation) -> error
         // val bottom_navigation = findViewById<BottomNavigationView>(R.id.bottom_navigation) -> 아래와 결과 같음
         val bottom_navigation : BottomNavigationView = findViewById(R.id.bottom_navigation)
-        bottom_navigation.setOnNavigationItemSelectedListener() { item ->
+        bottom_navigation.setOnNavigationItemSelectedListener { item ->
             when(item.itemId){
                 R.id.action_home ->{
                     val detailViewFragment = DetailViewFragment()
@@ -32,6 +34,9 @@ class MainActivity : AppCompatActivity() {
                 }
                 R.id.action_add_photo ->{
 
+                    if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+                        startActivity(Intent(this, AddPhotoActivity::class.java))
+                    }
                     true
                 }
                 R.id.action_favorite_alarm ->{
@@ -47,6 +52,11 @@ class MainActivity : AppCompatActivity() {
                 else -> false
             }
         }
+        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1)
+
+
     }
+
+
 }
 
