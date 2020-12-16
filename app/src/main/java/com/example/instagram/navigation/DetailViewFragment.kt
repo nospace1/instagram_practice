19128b8b6055c03cca3c21cf530c8e9ea0afe345
woojuan <dnwn010194@gmail.com>
2020-12-17 08:21:49 +0900

    뷰홀더에서 layout id import 성공

--- app/src/main/java/com/example/instagram/navigation/DetailViewFragment.kt
+++ app/src/main/java/com/example/instagram/navigation/DetailViewFragment.kt
@@ -4,19 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.instagram.R
 import com.example.instagram.navigation.model.ContentDTO
 import com.google.firebase.firestore.FirebaseFirestore
 
-class DetailViewFragment : Fragment() {
+class DetailViewFragment : Fragment(){
     var firestore : FirebaseFirestore? = null
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         var view = LayoutInflater.from(activity).inflate(R.layout.fragment_detail, container, false)
         firestore = FirebaseFirestore.getInstance()
         return view
@@ -50,7 +49,18 @@ class DetailViewFragment : Fragment() {
             var viewholder = (holder as CustomViewHolder).itemView
 
             // UserId
-            viewholder.detailview_profile_textview.text = contentDTOs!![position].userId //todo 오류 해결하기
+            viewholder.findViewById<TextView>(R.id.detailviewitem_profile_textview).text = contentDTOs!![position].userId
+
+            // Image
+            Glide.with(holder.itemView.context).load(contentDTOs!![position].imageUrl).into(viewholder.findViewById<ImageView>(R.id.detailviewitem_imageview_content))
+
+            // Explain of content
+            viewholder.findViewById<TextView>(R.id.detailviewitem_explain_textview).text = contentDTOs!![position].explain
+
+            // likes
+            viewholder.findViewById<TextView>(R.id.detailviewitem_favoritecount_textview).text = "Likes " + contentDTOs!![position].favoriteCount
+
+
         }
 
         override fun getItemCount(): Int {
