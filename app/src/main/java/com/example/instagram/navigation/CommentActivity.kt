cd418886c97b460e52aef074e1196544c4a05a77
woojuan <dnwn010194@gmail.com>
2020-12-22 08:27:46 +0900

    덧글 페이지 만들기

---
+++ app/src/main/java/com/example/instagram/navigation/CommentActivity.kt
@@ -0,0 +1,30 @@
+package com.example.instagram.navigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.instagram.R
+import com.example.instagram.navigation.model.ContentDTO
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_comment.*
+
+class CommentActivity : AppCompatActivity() {
+    var contentUid : String? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_comment)
+        contentUid = intent.getStringExtra("contentUid")
+
+        comment_btn_send?.setOnClickListener {
+            var comment = ContentDTO.Comment()
+            comment.userId = FirebaseAuth.getInstance().currentUser?.email
+            comment.uid = FirebaseAuth.getInstance().currentUser?.uid
+            comment.comment = comment_edit_message.text.toString()
+            comment.timestamp = System.currentTimeMillis()
+
+            FirebaseFirestore.getInstance().collection("images").document(contentUid!!).collection("comments").document().set(comment)
+            comment_edit_message.setText("")
+        }
+    }
+}
